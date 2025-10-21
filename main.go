d8255b2bd4ec890ac3e897088244b9d7ef4b5112
jcourtney5 <jesse.courtney5@gmail.com>
2025-10-21 00:29:46 -0600

    added initial repl loop in main.go

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) > 0 {
+			fmt.Println("Your command was:", words[0])
+		}
+	}
+
+	// Check for any error that might’ve occurred
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+	}
 }
